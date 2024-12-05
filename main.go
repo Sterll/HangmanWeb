cdd4515f5281d5f1f98449415c20551c2d01fad9
Yanis Benyacine <sterllytb@gmail.com>
2024-12-05 10:20:57 +0100

    v1.0

--- main.go
+++ main.go
@@ -1,25 +1,20 @@
-// hangman.go
-
 package main
 
 import (
 	"bufio"
 	"encoding/json"
-	"flag"
-	"fmt"
-	"io/ioutil"
+	"html/template"
+	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 	"unicode"
 )
-
-var filename string
-var playerName string
-var word string
-var wordtest []rune
-var hangmanStages []string
 
 type STATE int64
 
@@ -29,7 +24,14 @@ const (
 	END
 )
 
-var GAME_STATE STATE
+var (
+	filename      = "score.json"
+	wordFile      = "word.txt"
+	hangmanFile   = "hangman.txt"
+	hangmanStages []string
+	templates     *template.Template
+	scoresMutex   sync.Mutex
+)
 
 type Score struct {
 	Name  string `json:"name"`
@@ -40,75 +42,284 @@ type Scores struct {
 	Scores []Score `json:"scores"`
 }
 
-type GameState struct {
-	PlayerName   string        `json:"player_name"`
-	Word         string        `json:"word"`
-	WordTest     []rune        `json:"word_test"`
-	Decouverte   map[rune]bool `json:"decouverte"`
-	Erreurs      int           `json:"erreurs"`
-	GameState    STATE         `json:"game_state"`
-	PlayerWon    bool          `json:"player_won"`
-	Propositions []string      `json:"propositions"`
+type Game struct {
+	PlayerName   string
+	Word         string
+	WordTest     []rune
+	Decouverte   map[rune]bool
+	Erreurs      int
+	GameState    STATE
+	PlayerWon    bool
+	Propositions []string
+	MaxAttempts  int
 }
 
-func readScores(filename string) (Scores, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return Scores{}, err
-	}
-	defer file.Close()
+var games = make(map[string]*Game)
+var gamesMutex sync.Mutex
 
-	byteValue, _ := ioutil.ReadAll(file)
+func main() {
+	rand.Seed(time.Now().UnixNano())
 
-	var scores Scores
-	err = json.Unmarshal(byteValue, &scores)
+	var err error
+	hangmanStages, err = loadHangmanStages(hangmanFile)
 	if err != nil {
-		return Scores{}, err
+		log.Fatal(err)
 	}
 
-	return scores, nil
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/scores", scoresHandler)
+	http.HandleFunc("/game", gameHandler)
+	http.HandleFunc("/guess", guessHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("Serveur démarré sur http://localhost:8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Erreur lors du démarrage du serveur :", err)
+	}
 }
 
-func writeScores(filename string, scores Scores) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	jsonData, err := json.MarshalIndent(scores, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	_, err = file.Write(jsonData)
-	return err
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
-func getScore(scores Scores, playerName string) (int, error) {
-	for _, score := range scores.Scores {
-		if score.Name == playerName {
-			return score.Score, nil
-		}
+func scoresHandler(w http.ResponseWriter, r *http.Request) {
+	scores, err := readScores(filename)
+	if err != nil {
+		log.Println("Erreur lors de la lecture des scores :", err)
+		scores = Scores{Scores: []Score{}}
 	}
-	return 0, fmt.Errorf("Score non trouvé pour le joueur %s", playerName)
+
+	// Trier les scores par ordre décroissant
+	sort.Slice(scores.Scores, func(i, j int) bool {
+		return scores.Scores[i].Score > scores.Scores[j].Score
+	})
+
+	templates.ExecuteTemplate(w, "scores.html", scores.Scores)
 }
 
-func setScore(scores *Scores, playerName string, newScore int) {
-	for i, score := range scores.Scores {
-		if score.Name == playerName {
-			scores.Scores[i].Score = newScore
+func gameHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		pseudo := r.FormValue("pseudo")
+		if pseudo == "" {
+			http.Redirect(w, r, "/game", http.StatusSeeOther)
 			return
 		}
+
+		gameID := strconv.Itoa(rand.Intn(1000000))
+		game := startNewGame(pseudo)
+
+		gamesMutex.Lock()
+		games[gameID] = game
+		gamesMutex.Unlock()
+
+		http.Redirect(w, r, "/game?gameID="+gameID, http.StatusSeeOther)
+		return
 	}
 
-	scores.Scores = append(scores.Scores, Score{Name: playerName, Score: newScore})
+	gameID := r.URL.Query().Get("gameID")
+
+	var data map[string]interface{}
+
+	if gameID != "" {
+		gamesMutex.Lock()
+		game, exists := games[gameID]
+		gamesMutex.Unlock()
+
+		if exists {
+			data = map[string]interface{}{
+				"GameID":       gameID,
+				"Discovered":   string(game.WordTest),
+				"AttemptsLeft": game.MaxAttempts - game.Erreurs,
+				"HangmanStage": hangmanStages[game.Erreurs],
+				"Message":      r.URL.Query().Get("message"),
+				"PlayerName":   game.PlayerName,
+			}
+		} else {
+			data = map[string]interface{}{
+				"Message": "Partie non trouvée. Veuillez commencer une nouvelle partie.",
+			}
+		}
+	} else {
+		data = map[string]interface{}{
+			"Message": r.URL.Query().Get("message"),
+		}
+	}
+
+	templates.ExecuteTemplate(w, "game.html", data)
+}
+
+func guessHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/game", http.StatusSeeOther)
+		return
+	}
+
+	gameID := r.FormValue("gameID")
+	input := r.FormValue("guess")
+
+	gamesMutex.Lock()
+	game, exists := games[gameID]
+	gamesMutex.Unlock()
+
+	if !exists {
+		http.Redirect(w, r, "/game", http.StatusSeeOther)
+		return
+	}
+
+	input = strings.TrimSpace(input)
+	input = strings.ToLower(input)
+
+	if input == "" {
+		http.Redirect(w, r, "/game?gameID="+gameID, http.StatusSeeOther)
+		return
+	}
+
+	if containsString(game.Propositions, input) {
+		http.Redirect(w, r, "/game?gameID="+gameID+"&message=Déjà proposé", http.StatusSeeOther)
+		return
+	}
+
+	game.Propositions = append(game.Propositions, input)
+
+	if len(input) > 1 {
+		if input == game.Word {
+			game.PlayerWon = true
+			game.WordTest = []rune(game.Word)
+			game.GameState = END
+		} else {
+			game.Erreurs += 2
+		}
+	} else {
+		letter := rune(input[0])
+		if !unicode.IsLetter(letter) {
+			http.Redirect(w, r, "/game?gameID="+gameID+"&message=Lettre invalide", http.StatusSeeOther)
+			return
+		}
+
+		if game.Decouverte[letter] {
+			http.Redirect(w, r, "/game?gameID="+gameID+"&message=Lettre déjà proposée", http.StatusSeeOther)
+			return
+		}
+
+		game.Decouverte[letter] = true
+
+		if strings.ContainsRune(game.Word, letter) {
+			for i, l := range game.Word {
+				if l == letter {
+					game.WordTest[i] = letter
+				}
+			}
+		} else {
+			game.Erreurs++
+		}
+	}
+
+	if !containsRune(game.WordTest, '_') {
+		game.PlayerWon = true
+		game.GameState = END
+	} else if game.Erreurs >= game.MaxAttempts {
+		game.GameState = END
+	}
+
+	if game.GameState == END {
+		if game.PlayerWon {
+			scoresMutex.Lock()
+			scores, err := readScores(filename)
+			if err != nil {
+				log.Println("Erreur lors de la lecture des scores :", err)
+				scores = Scores{Scores: []Score{}}
+			}
+			scoreOfPlayer, _ := getScore(scores, game.PlayerName)
+			setScore(&scores, game.PlayerName, scoreOfPlayer+1)
+			err = writeScores(filename, scores)
+			if err != nil {
+				log.Println("Erreur lors de l'écriture des scores :", err)
+			}
+			scoresMutex.Unlock()
+
+			http.Redirect(w, r, "/game?message=Félicitations! Vous avez gagné!", http.StatusSeeOther)
+		} else {
+			http.Redirect(w, r, "/game?message=Dommage! Vous avez perdu. Le mot était: "+game.Word, http.StatusSeeOther)
+		}
+
+		gamesMutex.Lock()
+		delete(games, gameID)
+		gamesMutex.Unlock()
+	} else {
+		gamesMutex.Lock()
+		games[gameID] = game
+		gamesMutex.Unlock()
+		http.Redirect(w, r, "/game?gameID="+gameID, http.StatusSeeOther)
+	}
+}
+
+func startNewGame(playerName string) *Game {
+	mots, err := loadWords(wordFile)
+	if err != nil {
+		log.Println("Erreur lors du chargement des mots :", err)
+		mots = []string{"erreur"}
+	}
+
+	index := rand.Intn(len(mots))
+	word := strings.ToLower(mots[index])
+	wordTest := make([]rune, len(word))
+	for i := range word {
+		wordTest[i] = '_'
+	}
+
+	revealCount := len(word)/2 - 1
+	if revealCount < 0 {
+		revealCount = 0
+	}
+
+	revealedIndices := make(map[int]bool)
+	for len(revealedIndices) < revealCount {
+		idx := rand.Intn(len(word))
+		if !revealedIndices[idx] && wordTest[idx] == '_' {
+			wordTest[idx] = rune(word[idx])
+			revealedIndices[idx] = true
+		}
+	}
+
+	return &Game{
+		PlayerName:   playerName,
+		Word:         word,
+		WordTest:     wordTest,
+		Decouverte:   make(map[rune]bool),
+		Erreurs:      0,
+		GameState:    PLAYING,
+		PlayerWon:    false,
+		Propositions: []string{},
+		MaxAttempts:  len(hangmanStages) - 1,
+	}
+}
+
+func loadWords(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var mots []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		ligne := scanner.Text()
+		mots = append(mots, ligne)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return mots, nil
 }
 
 func loadHangmanStages(filename string) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
-		return nil, fmt.Errorf("Erreur lors de l'ouverture du fichier %s : %v", filename, err)
+		return nil, err
 	}
 	defer file.Close()
 
@@ -136,289 +347,58 @@ func loadHangmanStages(filename string) ([]string, error) {
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("Erreur lors de la lecture du fichier %s : %v", filename, err)
+		return nil, err
 	}
 	return stages, nil
 }
 
-func main() {
-	filename = "score.json"
-
-	var startWith string
-	flag.StringVar(&startWith, "startWith", "", "Commencer le jeu avec le fichier de sauvegarde spécifié")
-	flag.Parse()
-
-	var err error
-	hangmanStages, err = loadHangmanStages("hangman.txt")
+func readScores(filename string) (Scores, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var mots []string
-	file, err := os.Open("word.txt")
-	if err != nil {
-		fmt.Println("Erreur lors de l'ouverture du fichier des mots :", err)
-		return
+		return Scores{}, err
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		ligne := scanner.Text()
-		mots = append(mots, ligne)
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Erreur lors de la lecture du fichier des mots :", err)
-		return
+	decoder := json.NewDecoder(file)
+	var scores Scores
+	err = decoder.Decode(&scores)
+	if err != nil {
+		return Scores{}, err
 	}
 
-	if startWith != "" {
-		savedGame, err := loadGame(startWith)
-		if err != nil {
-			fmt.Println("Erreur lors du chargement de la sauvegarde :", err)
-			return
-		}
-		playerName = savedGame.PlayerName
-		word = savedGame.Word
-		wordtest = savedGame.WordTest
-		GAME_STATE = savedGame.GameState
-
-		scores, err := readScores(filename)
-		if err != nil {
-			fmt.Println("Erreur lors de la lecture du fichier des scores :", err)
-			scores = Scores{Scores: []Score{}}
-		}
-
-		_, err2 := getScore(scores, playerName)
-		if err2 != nil {
-			newScore := Score{
-				Name:  playerName,
-				Score: 0,
-			}
-			scores.Scores = append(scores.Scores, newScore)
-			err = writeScores(filename, scores)
-			if err != nil {
-				fmt.Println("Erreur lors de l'écriture dans le fichier des scores :", err)
-			}
-		}
-
-		play(&savedGame.Decouverte, &savedGame.Erreurs, savedGame.PlayerWon, &savedGame.Propositions, mots)
-	} else {
-		fmt.Println("Bienvenue dans le jeu du Pendu")
-		fmt.Println("Veuillez entrer votre pseudo :")
-		fmt.Scan(&playerName)
-
-		scores, err := readScores(filename)
-		if err != nil {
-			fmt.Println("Erreur lors de la lecture du fichier des scores :", err)
-			scores = Scores{Scores: []Score{}}
-		}
-
-		_, err2 := getScore(scores, playerName)
-		if err2 != nil {
-			newScore := Score{
-				Name:  playerName,
-				Score: 0,
-			}
-			scores.Scores = append(scores.Scores, newScore)
-			err = writeScores(filename, scores)
-			if err != nil {
-				fmt.Println("Erreur lors de l'écriture dans le fichier des scores :", err)
-			}
-		}
-
-		for {
-			welcome(scores, mots)
-		}
-	}
+	return scores, nil
 }
 
-func welcome(scores Scores, mots []string) {
-	fmt.Println("\nVeuillez choisir une option ci-dessous :")
-	fmt.Println("1. Commencer une partie")
-	fmt.Println("2. Voir les règles du jeu")
-	fmt.Println("3. Voir les scores")
-	fmt.Println("4. Quitter le jeu")
-
-	var choice int
-	fmt.Scan(&choice)
-	switch choice {
-	case 1:
-		decouverte := make(map[rune]bool)
-		erreurs := 0
-		playerWon := false
-		propositions := []string{}
-		play(&decouverte, &erreurs, playerWon, &propositions, mots)
-	case 2:
-		fmt.Println("\nRègles du jeu :")
-		fmt.Println("Un mot sera choisi aléatoirement parmi une liste prédéfinie.")
-		fmt.Println("Vous avez un certain nombre de vies pour le deviner, correspondant au nombre d'étapes du dessin du pendu.")
-		fmt.Println("Au début de chaque partie, certaines lettres seront déjà découvertes.")
-		fmt.Println("Vous pouvez proposer soit une lettre, soit un mot (au moins deux caractères).")
-		fmt.Println("Si vous proposez un mot et qu'il est incorrect, le compteur de tentatives diminue de 2.")
-		fmt.Println("Vous ne pouvez pas proposer la même lettre deux fois.")
-		fmt.Println("Vous pouvez taper 'STOP' à tout moment pour sauvegarder et quitter la partie.")
-		fmt.Println("Bonne chance !\n")
-	case 3:
-		fmt.Println("\nScores actuels :")
-		for _, sc := range scores.Scores {
-			fmt.Println(sc.Name, ":", sc.Score)
-		}
-	case 4:
-		fmt.Println("Merci d'avoir joué ! À bientôt.")
-		os.Exit(0)
-	default:
-		fmt.Println("Choix invalide. Veuillez sélectionner une option de 1 à 4.")
+func writeScores(filename string, scores Scores) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
 	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(scores)
+	return err
 }
 
-func play(decouverte *map[rune]bool, erreurs *int, playerWon bool, propositions *[]string, mots []string) {
-	GAME_STATE = PLAYING
-	tentativesMax := len(hangmanStages) - 1
-
-	if word == "" {
-		index := rand.Intn(len(mots))
-		word = strings.ToLower(mots[index])
-		wordtest = make([]rune, len(word))
-		for i := range word {
-			wordtest[i] = '_'
-		}
-
-		// Calcul du nombre de lettres à révéler
-		revealCount := len(word)/2 - 1
-		if revealCount < 0 {
-			revealCount = 0
-		}
-
-		// Révélation aléatoire des lettres
-		revealedIndices := make(map[int]bool)
-		for len(revealedIndices) < revealCount {
-			idx := rand.Intn(len(word))
-			if !revealedIndices[idx] && wordtest[idx] == '_' {
-				wordtest[idx] = rune(word[idx])
-				revealedIndices[idx] = true
-			}
+func getScore(scores Scores, playerName string) (int, error) {
+	for _, score := range scores.Scores {
+		if score.Name == playerName {
+			return score.Score, nil
 		}
 	}
+	return 0, nil
+}
 
-	reader := bufio.NewReader(os.Stdin)
-
-	for GAME_STATE == PLAYING {
-		fmt.Println("\n-----------------------------")
-		fmt.Println(hangmanStages[*erreurs])
-
-		fmt.Printf("\nMot à deviner : ")
-		for _, r := range wordtest {
-			fmt.Printf("%c ", r)
-		}
-		fmt.Printf("\nLettres déjà devinées : ")
-		letters := make([]rune, 0, len(*decouverte))
-		for l := range *decouverte {
-			letters = append(letters, l)
-		}
-		sort.Slice(letters, func(i, j int) bool { return letters[i] < letters[j] })
-		for _, l := range letters {
-			fmt.Printf("%c ", l)
-		}
-		fmt.Printf("\nTentatives restantes : %d\n", tentativesMax-*erreurs)
-		fmt.Println("Veuillez entrer une lettre ou un mot (ou 'STOP' pour sauvegarder et quitter) :")
-
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-
-		if strings.ToUpper(input) == "STOP" {
-			err := saveGame("save.txt", decouverte, *erreurs, playerWon, propositions)
-			if err != nil {
-				fmt.Println("Erreur lors de la sauvegarde du jeu :", err)
-			} else {
-				fmt.Println("Jeu sauvegardé. À bientôt !")
-			}
-			os.Exit(0)
-		}
-
-		if containsString(*propositions, strings.ToLower(input)) {
-			fmt.Println("Vous avez déjà proposé cette lettre ou ce mot.")
-			continue
-		}
-
-		*propositions = append(*propositions, strings.ToLower(input))
-
-		if len(input) > 1 {
-			if strings.ToLower(input) == word {
-				fmt.Println("\nFélicitations, vous avez deviné le mot !")
-				fmt.Printf("Le mot était : %s\n", word)
-				GAME_STATE = END
-				playerWon = true
-			} else {
-				fmt.Println("Ce n'est pas le bon mot.")
-				*erreurs += 2
-			}
-		} else if len(input) == 1 {
-			if !unicode.IsLetter(rune(input[0])) {
-				fmt.Println("Veuillez entrer une lettre valide.")
-				continue
-			}
-
-			letter := rune(input[0])
-			letter = unicode.ToLower(letter)
-
-			if (*decouverte)[letter] {
-				fmt.Println("Vous avez déjà deviné cette lettre.")
-				continue
-			}
-
-			(*decouverte)[letter] = true
-
-			if strings.ContainsRune(word, letter) {
-				fmt.Println("Bonne réponse !")
-				for i, l := range word {
-					if l == letter {
-						wordtest[i] = letter
-					}
-				}
-			} else {
-				fmt.Println("Mauvaise réponse.")
-				*erreurs++
-			}
-		} else {
-			fmt.Println("Entrée invalide.")
-			continue
-		}
-
-		if !containsRune(wordtest, '_') {
-			fmt.Println("\nFélicitations, vous avez gagné !")
-			fmt.Printf("Le mot était : %s\n", word)
-			GAME_STATE = END
-			playerWon = true
-		}
-
-		if *erreurs >= tentativesMax {
-			fmt.Println("\n-----------------------------")
-			fmt.Println(hangmanStages[*erreurs])
-			fmt.Println("\nVous avez perdu.")
-			fmt.Printf("Le mot était : %s\n", word)
-			GAME_STATE = END
-		}
-	}
-
-	if GAME_STATE == END && playerWon {
-		scores, err := readScores(filename)
-		if err != nil {
-			fmt.Println("Impossible de sauvegarder votre score !")
+func setScore(scores *Scores, playerName string, newScore int) {
+	for i, score := range scores.Scores {
+		if score.Name == playerName {
+			scores.Scores[i].Score = newScore
 			return
 		}
-		scoreOfPlayer, _ := getScore(scores, playerName)
-		setScore(&scores, playerName, scoreOfPlayer+1)
-		err = writeScores(filename, scores)
-		if err != nil {
-			fmt.Println("Impossible de sauvegarder votre score !")
-		}
 	}
-
-	word = ""
-	wordtest = []rune{}
-	GAME_STATE = WAITING
+	scores.Scores = append(scores.Scores, Score{Name: playerName, Score: newScore})
 }
 
 func containsRune(slice []rune, r rune) bool {
@@ -437,41 +417,4 @@ func containsString(slice []string, s string) bool {
 		}
 	}
 	return false
-}
-
-func saveGame(filename string, decouverte *map[rune]bool, erreurs int, playerWon bool, propositions *[]string) error {
-	gameState := GameState{
-		PlayerName:   playerName,
-		Word:         word,
-		WordTest:     wordtest,
-		Decouverte:   *decouverte,
-		Erreurs:      erreurs,
-		GameState:    GAME_STATE,
-		PlayerWon:    playerWon,
-		Propositions: *propositions,
-	}
-
-	data, err := json.MarshalIndent(gameState, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(filename, data, 0644)
-	return err
-}
-
-func loadGame(filename string) (GameState, error) {
-	var gameState GameState
-
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return gameState, err
-	}
-
-	err = json.Unmarshal(data, &gameState)
-	if err != nil {
-		return gameState, err
-	}
-
-	return gameState, nil
 }
